6c1698be7ba2590a03f70938d44469936637b935
Mahinich <andrey.mahinitch@gmail.com>
2023-05-25 03:26:25 +0300

    some additions

--- sprint13/homework/unit_13/unit_13.js
+++ sprint13/homework/unit_13/unit_13.js
@@ -488,7 +488,7 @@ function f20() {
 
     for (let key in a20) {
         for (let i = 0; i < a20[key].length; i++) {
-            if (a20[key][i][1] === 0) {
+            if (a20[key][i][1] === 2) {
                 out += a20[key][i][0] + ' ';
             }
         }
@@ -497,4 +497,4 @@ function f20() {
     document.querySelector('.out-20').textContent = out;
 }
 
-document.querySelector('.b-20').onclick = f20
+document.querySelector('.b-20').onclick = f20;
